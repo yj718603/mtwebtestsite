e8b441835214e50b22e407b9e328e79cd7b16e62
yj718603 <yj718603@hotmail.com>
2025-02-25 16:59:18 +0800

    first comm

---
+++ src/assets/config/supportedLangs.ts
@@ -0,0 +1,14 @@
+// supportedLangs.ts
+export const supportedLangs = [
+    { code: "en", name: "English" },
+    { code: "zh-Hans", name: "中文（简体）" },
+    { code: "zh-Hant", name: "中文（繁体）" },
+    { code: "es", name: "español" },
+    { code: "fr", name: "français" },
+    { code: "pt", name: "português" },
+    { code: "ru", name: "русский" },
+    { code: "de", name: "Deutsch" },
+    { code: "ja", name: "日本語" },
+    { code: "it", name: "italiano" },
+    { code: "ko", name: "한국어" },
+];
